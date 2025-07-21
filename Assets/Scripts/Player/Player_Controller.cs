414d01cb4828318bd405ceb85176adf9a3a79d21
Miisane <azarmisan@gmail.com>
2025-07-22 02:48:21 +0800

    Fix: updated old input system to new input system
    no more fuck ass old input manager

--- Assets/Scripts/Player/Player_Controller.cs
+++ Assets/Scripts/Player/Player_Controller.cs
@@ -1,131 +1,184 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public enum Player_Type { Luthe, Cherie }
 
 public class Player_Controller : MonoBehaviour
 {
     [SerializeField] private Player_Type player_type;
-    [SerializeField] private bool use_controller = false; 
+    [SerializeField] private InputSystem_Actions inputActions;
+    
     private Player_Movement movement_script;
-
     private bool is_in_push_mode = false;
+    
+    private Vector2 moveInput;
+    private bool jumpPressed;
+    private bool interactPressed;
+    private bool interactHeld;
+    private float rotationInput;
 
+    void Awake()
+    {
+        inputActions = new InputSystem_Actions();
+    }
 
     void Start()
     {
         movement_script = GetComponent<Player_Movement>();
+        SetupInputCallbacks();
+    }
+
+    void OnEnable()
+    {
+        inputActions?.PlayerInputActions.Enable();
+    }
+
+    void OnDisable()
+    {
+        inputActions?.PlayerInputActions.Disable();
+    }
+
+    void OnDestroy()
+    {
+        CleanupInputCallbacks();
+        inputActions?.Dispose();
+    }
+
+    private void SetupInputCallbacks()
+    {
+        inputActions.PlayerInputActions.Move.performed += OnMove;
+        inputActions.PlayerInputActions.Move.canceled += OnMove;
+        inputActions.PlayerInputActions.Jump.performed += OnJump;
+        inputActions.PlayerInputActions.Interact.performed += OnInteract;
+        inputActions.PlayerInputActions.InteractHeld.performed += OnInteractHeld;
+        inputActions.PlayerInputActions.InteractHeld.canceled += OnInteractHeld;
+        inputActions.PlayerInputActions.Rotate.performed += OnRotate;
+        inputActions.PlayerInputActions.Rotate.canceled += OnRotate;
+    }
+
+    private void CleanupInputCallbacks()
+    {
+        if (inputActions != null)
+        {
+            inputActions.PlayerInputActions.Move.performed -= OnMove;
+            inputActions.PlayerInputActions.Move.canceled -= OnMove;
+            inputActions.PlayerInputActions.Jump.performed -= OnJump;
+            inputActions.PlayerInputActions.Interact.performed -= OnInteract;
+            inputActions.PlayerInputActions.InteractHeld.performed -= OnInteractHeld;
+            inputActions.PlayerInputActions.InteractHeld.canceled -= OnInteractHeld;
+            inputActions.PlayerInputActions.Rotate.performed -= OnRotate;
+            inputActions.PlayerInputActions.Rotate.canceled -= OnRotate;
+        }
+    }
+
+    private void OnMove(InputAction.CallbackContext context)
+    {
+        if (IsCorrectDevice(context.control.device))
+        {
+            moveInput = context.ReadValue<Vector2>();
+        }
+        else if (context.canceled)
+        {
+            if (IsPlayerKeyboard(context.control.device))
+            {
+                moveInput = Vector2.zero;
+            }
+        }
+    }
+
+    private void OnJump(InputAction.CallbackContext context)
+    {
+        if (IsCorrectDevice(context.control.device))
+            jumpPressed = true;
+    }
+
+    private void OnInteract(InputAction.CallbackContext context)
+    {
+        if (IsCorrectDevice(context.control.device))
+            interactPressed = true;
+    }
+
+    private void OnInteractHeld(InputAction.CallbackContext context)
+    {
+        if (IsCorrectDevice(context.control.device))
+            interactHeld = context.ReadValueAsButton();
+    }
+
+    private void OnRotate(InputAction.CallbackContext context)
+    {
+        if (IsCorrectDevice(context.control.device))
+            rotationInput = context.ReadValue<float>();
+    }
+
+    private bool IsCorrectDevice(InputDevice device)
+    {
+        if (device is Keyboard)
+        {
+            return IsCorrectKeyboardInput(device as Keyboard);
+        }
+        else if (device is Gamepad gamepad)
+        {
+            var gamepads = Gamepad.all;
+            
+            if (player_type == Player_Type.Luthe)
+            {
+                return gamepads.Count > 0 && gamepad == gamepads[0];
+            }
+            else
+            {
+                return gamepads.Count > 1 && gamepad == gamepads[1];
+            }
+        }
+        
+        return false;
+    }
+
+    private bool IsPlayerKeyboard(InputDevice device)
+    {
+        return device is Keyboard;
+    }
+
+    private bool IsCorrectKeyboardInput(Keyboard keyboard)
+    {
+        if (player_type == Player_Type.Luthe)
+        {
+            return keyboard.wKey.isPressed || keyboard.aKey.isPressed || 
+                   keyboard.sKey.isPressed || keyboard.dKey.isPressed ||
+                   keyboard.spaceKey.isPressed || keyboard.eKey.isPressed ||
+                   keyboard.qKey.isPressed || keyboard.rKey.isPressed;
+        }
+        else
+        {
+            return keyboard.upArrowKey.isPressed || keyboard.downArrowKey.isPressed ||
+                   keyboard.leftArrowKey.isPressed || keyboard.rightArrowKey.isPressed ||
+                   keyboard.rightShiftKey.isPressed || keyboard.enterKey.isPressed ||
+                   keyboard.commaKey.isPressed || keyboard.periodKey.isPressed;
+        }
     }
 
     void Update()
     {
-        Vector2 input = Get_Input();
-
         if (!is_in_push_mode)
         {
-            movement_script.Move(input);
+            movement_script.Move(moveInput);
 
-            if (GetJumpInput())
+            if (jumpPressed)
+            {
                 movement_script.Jump();
+                jumpPressed = false;
+            }
         }
         else
         {
             movement_script.Move(Vector2.zero);
         }
 
-        if (GetInteractInput())
+        if (interactPressed)
         {
-
+            interactPressed = false;
         }
     }
 
-    private Vector2 Get_Input()
-    {
-        if (use_controller)
-        {
-            try
-            {
-                if (player_type == Player_Type.Luthe)
-                {
-                    float h = Input.GetAxis("Joy1_Horizontal");
-                    float v = Input.GetAxis("Joy1_Vertical");
-
-                    if (Mathf.Abs(h) < 0.3f) h = 0f;
-                    if (Mathf.Abs(v) < 0.3f) v = 0f;
-
-                    if (Mathf.Abs(h) > 0.1f || Mathf.Abs(v) > 0.1f)
-                        return new Vector2(h, v);
-                }
-                else
-                {
-                    float h = Input.GetAxis("Joy2_Horizontal");
-                    float v = Input.GetAxis("Joy2_Vertical");
-
-                    if (Mathf.Abs(h) < 0.3f) h = 0f;
-                    if (Mathf.Abs(v) < 0.3f) v = 0f;
-
-                    if (Mathf.Abs(h) > 0.1f || Mathf.Abs(v) > 0.1f)
-                        return new Vector2(h, v);
-                }
-            }
-            catch
-            {
-            }
-        }
-
-        return Get_Keyboard_Input();
-    }
-    private Vector2 Get_Keyboard_Input()
-    {
-        float horizontal = 0f;
-        float vertical = 0f;
-
-        if (player_type == Player_Type.Luthe)
-        {
-            if (Input.GetKey(KeyCode.A)) horizontal = -1f;
-            if (Input.GetKey(KeyCode.D)) horizontal = 1f;
-            if (Input.GetKey(KeyCode.W)) vertical = 1f;
-            if (Input.GetKey(KeyCode.S)) vertical = -1f;
-        }
-        else
-        {
-            if (Input.GetKey(KeyCode.LeftArrow)) horizontal = -1f;
-            if (Input.GetKey(KeyCode.RightArrow)) horizontal = 1f;
-            if (Input.GetKey(KeyCode.UpArrow)) vertical = 1f;
-            if (Input.GetKey(KeyCode.DownArrow)) vertical = -1f;
-        }
-
-        return new Vector2(horizontal, vertical);
-    }
-
-    private bool GetJumpInput()
-    {
-        if (player_type == Player_Type.Luthe)
-        {
-            if (Input.GetKeyDown("joystick 1 button 0")) return true;
-            return Input.GetKeyDown(KeyCode.Space);
-        }
-        else
-        {
-            if (Input.GetKeyDown("joystick 2 button 0")) return true;
-            return Input.GetKeyDown(KeyCode.RightShift);
-        }
-    }
-
-    private bool GetInteractInput()
-    {
-        if (player_type == Player_Type.Luthe)
-        {
-            if (Input.GetKeyDown("joystick 1 button 2")) return true;
-            return Input.GetKeyDown(KeyCode.E);
-        }
-        else
-        {
-            if (Input.GetKeyDown("joystick 2 button 2")) return true;
-            return Input.GetKeyDown(KeyCode.Return);
-        }
-    }
-
-    // ----------- INTERACTION SYSTEM UTILS -------------   
     public Player_Type Get_Player_Type()
     {
         return player_type;
@@ -133,13 +186,14 @@ public class Player_Controller : MonoBehaviour
 
     public Vector3 Get_Movement_Input()
     {
-        Vector2 input = Get_Input();
-        return new Vector3(input.x, 0, input.y);
+        return new Vector3(moveInput.x, 0, moveInput.y);
     }
 
     public bool Get_Interact_Input()
     {
-        return GetInteractInput();
+        bool result = interactPressed;
+        interactPressed = false;
+        return result;
     }
 
     public void Set_Push_Mode(bool pushing)
@@ -149,43 +203,11 @@ public class Player_Controller : MonoBehaviour
 
     public bool Get_Interact_Held()
     {
-        if (player_type == Player_Type.Luthe)
-        {
-            if (Input.GetKey("joystick 1 button 2")) return true;
-            return Input.GetKey(KeyCode.E);
-        }
-        else
-        {
-            if (Input.GetKey("joystick 2 button 2")) return true;
-            return Input.GetKey(KeyCode.Return);
-        }
+        return interactHeld;
     }
     
     public float Get_Rotation_Input()
-{
-    float rotation = 0f;
-    
-    if (player_type == Player_Type.Luthe)
     {
-        // Controller 1 - L2/R2 buttons
-        if (Input.GetKey("joystick 1 button 6")) rotation = -1f;  // L2 = rotate left
-        if (Input.GetKey("joystick 1 button 7")) rotation = 1f;   // R2 = rotate right
-        
-        // Keyboard fallback
-        if (Input.GetKey(KeyCode.Q)) rotation = -1f;  // Q = rotate left
-        if (Input.GetKey(KeyCode.R)) rotation = 1f;   // R = rotate right
+        return rotationInput;
     }
-    else
-    {
-        // Controller 2 - L2/R2 buttons
-        if (Input.GetKey("joystick 2 button 6")) rotation = -1f;  // L2 = rotate left
-        if (Input.GetKey("joystick 2 button 7")) rotation = 1f;   // R2 = rotate right
-        
-        // Keyboard fallback
-        if (Input.GetKey(KeyCode.Comma)) rotation = -1f;   // , = rotate left
-        if (Input.GetKey(KeyCode.Period)) rotation = 1f;   // . = rotate right
-    }
-    
-    return rotation;
-}
 }

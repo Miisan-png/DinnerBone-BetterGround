155c90fd306bcd77442d0d0d2888803b01564a25
Miisane <azarmisan@gmail.com>
2025-08-08 00:46:04 +0800

    add fan

--- Assets/Scripts/Fan_Drag_Player.cs
+++ Assets/Scripts/Fan_Drag_Player.cs
@@ -2,15 +2,47 @@ using UnityEngine;
 
 public class Fan_Drag_Player : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] private float drag_force = 5f;
+    [SerializeField] private Transform fan_object;
+    [SerializeField] private LayerMask player_layer = -1;
+    
+    void OnTriggerStay(Collider other)
+    {
+        if (IsInLayerMask(other.gameObject, player_layer))
+        {
+            Player_Controller player = other.GetComponent<Player_Controller>();
+            if (player != null && fan_object != null)
+            {
+                Vector3 player_pos = player.transform.position;
+                Vector3 fan_pos = fan_object.position;
+                
+                Vector3 drag_direction = (fan_pos - player_pos).normalized;
+                
+                CharacterController cc = player.GetComponent<CharacterController>();
+                if (cc != null)
+                {
+                    cc.Move(drag_direction * drag_force * Time.deltaTime);
+                }
+                else
+                {
+                    player.transform.position += drag_direction * drag_force * Time.deltaTime;
+                }
+            }
+        }
+    }
+    
+    private bool IsInLayerMask(GameObject obj, LayerMask layerMask)
+    {
+        return (layerMask.value & (1 << obj.layer)) != 0;
+    }
+    
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Collider trigger = GetComponent<Collider>();
+        if (trigger == null)
+        {
+            trigger = gameObject.AddComponent<BoxCollider>();
+        }
+        trigger.isTrigger = true;
     }
 }

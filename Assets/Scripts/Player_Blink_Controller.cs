0c6ac3d216a6ba79744ef2c6231b340df613ab00
Miisane <azarmisan@gmail.com>
2025-08-08 18:27:46 +0800

    add: blinking

--- Assets/Scripts/Player_Blink_Controller.cs
+++ Assets/Scripts/Player_Blink_Controller.cs
@@ -2,15 +2,45 @@ using UnityEngine;
 
 public class Player_Blink_Controller : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public SkinnedMeshRenderer targetMesh;
+    public Material openEyesMaterial;
+    public Material closedEyesMaterial;
+    public float blinkInterval = 3f;
+    public float blinkDuration = 0.1f;
+
+    private float blinkTimer;
+    private bool isBlinking;
+    private float blinkEndTimer;
+
     void Start()
     {
-        
+        if (targetMesh == null)
+            targetMesh = GetComponent<SkinnedMeshRenderer>();
+            
+        blinkTimer = blinkInterval;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (isBlinking)
+        {
+            blinkEndTimer -= Time.deltaTime;
+            if (blinkEndTimer <= 0)
+            {
+                targetMesh.material = openEyesMaterial;
+                isBlinking = false;
+                blinkTimer = blinkInterval;
+            }
+        }
+        else
+        {
+            blinkTimer -= Time.deltaTime;
+            if (blinkTimer <= 0)
+            {
+                targetMesh.material = closedEyesMaterial;
+                isBlinking = true;
+                blinkEndTimer = blinkDuration;
+            }
+        }
     }
 }
